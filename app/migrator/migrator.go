efb070bce13afcd4fe08dca858681927e6ae4b42
Arham Abiyan <arham@sinbad.co.id>
2021-06-27 09:20:06 +0700

    fix template and igonre migration file

--- app/migrator/migrator.go
+++ app/migrator/migrator.go
@@ -25,26 +25,26 @@ type Migrator struct {
 	Migrations map[string]*Migration
 }
 
-var migrator = &Migrator{
+var Agent = &Migrator{
 	Versions:   []string{},
 	Migrations: map[string]*Migration{},
 }
 
 func Init(db *sql.DB) (*Migrator, error) {
-	migrator.db = db
+	Agent.db = db
 
 	// get past migration
 	if _, err := db.Exec(`CREATE TABLE IF NOT EXISTS schema_migrations (version varchar(255));`); err != nil {
 		fmt.Println("Unable to create `schema_migrations` table", err)
 
-		return migrator, err
+		return Agent, err
 	}
 
 	// Find out past migrations
 	rows, err := db.Query("SELECT version FROM `schema_migrations`;")
 
 	if err != nil {
-		return migrator, err
+		return Agent, err
 	}
 
 	defer rows.Close()
@@ -54,15 +54,15 @@ func Init(db *sql.DB) (*Migrator, error) {
 		var version string
 		err := rows.Scan(&version)
 		if err != nil {
-			return migrator, err
+			return Agent, err
 		}
 
-		if migrator.Migrations[version] != nil {
-			migrator.Migrations[version].done = true
+		if Agent.Migrations[version] != nil {
+			Agent.Migrations[version].done = true
 		}
 	}
 
-	return migrator, err
+	return Agent, err
 }
 
 func Create(name string) error {
